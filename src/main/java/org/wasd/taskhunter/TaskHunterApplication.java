57cee075c39621685ff2168381ef5223bdab18e7
wasd <kirshvedov13@gmail.com>
2023-12-24 23:01:33 +0200

    init project

---
+++ src/main/java/org/wasd/taskhunter/TaskHunterApplication.java
@@ -0,0 +1,13 @@
+package org.wasd.taskhunter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskHunterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskHunterApplication.class, args);
+    }
+
+}
